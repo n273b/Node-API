d42c073d800195804a2515a70dae34bc00971b89
n273b <nishthabarnwal273@gmail.com>
2024-03-01 20:13:12 +0530

    Added DELETE request to delete the data from the database

--- server.js
+++ server.js
@@ -61,6 +61,23 @@ app.put('/products/:id', async(req, res) => {
     }
 })
 
+// delete a product
+
+app.delete('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const product = await Product.findByIdAndDelete(id)
+        // we cannot find any product in database 
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }   
+        res.status(200).json(product)
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 mongoose.
 connect('mongodb+srv://nishthabarnwal273:clvchfsCJzDPYymr@cluster0.0vjvjf9.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => {
