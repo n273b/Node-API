a64ac99c85858cdfb657506eda91e7b863d9c73c
n273b <nishthabarnwal273@gmail.com>
2024-03-01 19:40:31 +0530

    Added GET and POST requests to the server for the products as well as each product

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose');
+const Product = require('./models/productModel')
+const app = express()
+
+app.use(express.json())
 
 // routes
 
@@ -11,6 +14,35 @@ app.get('/', function (req, res) {
 app.get('/blog', function (req, res) {
     res.send('Hello Blog. My name is ninja.')
   })
+
+app.get('/products', async(req, res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const product = await Product.findById(id)
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/products', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+    }
+})
 
 mongoose.
 connect('mongodb+srv://nishthabarnwal273:clvchfsCJzDPYymr@cluster0.0vjvjf9.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0')
